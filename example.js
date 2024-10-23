ff49d68fa553617b6277b68dae05e667d5d28b94
bimlu <robinrawat7017@gmail.com>
2024-10-24 02:12:54 +0530

    Use example and update doc

--- example.js
+++ example.js
@@ -13,6 +13,17 @@ hlsVod.load()
             20000,
             'https://maitv-vod.lab.eyevinn.technology/ads/apotea-15s.mp4/master.m3u8'
         );
+
+        // return hlsVod.insertBumper(
+        //     'https://maitv-vod.lab.eyevinn.technology/ads/apotea-15s.mp4/master.m3u8'
+        // );
+
+        // return hlsVod.insertInterstitialAt(
+        //     30000,
+        //     "id",
+        //     'https://maitv-vod.lab.eyevinn.technology/ads/apotea-15s.mp4/master.m3u8',
+        //     true
+        // );
     })
     .then(() => {
         const mediaManifest = hlsVod.getMediaManifest(4928000);
