ec8758e7b8a8d80a8dc0294ae524b6e4b2e54473
bimlu <robinrawat7017@gmail.com>
2024-10-24 01:42:38 +0530

    Add new content

--- example.js
+++ example.js
@@ -5,10 +5,12 @@ const hlsVod = new HLSSpliceVod(
     'https://maitv-vod.lab.eyevinn.technology/stswe17-ozer.mp4/master.m3u8'
 );
 
+console.log('hlsVod:', hlsVod)
+
 hlsVod.load()
     .then(() => {
         return hlsVod.insertAdAt(
-            35000,
+            20000,
             'https://maitv-vod.lab.eyevinn.technology/ads/apotea-15s.mp4/master.m3u8'
         );
     })
